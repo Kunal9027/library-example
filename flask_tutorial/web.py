088d00ed8fcbab279ee61ca034ee17d3c378a2b5
Kunal9027 <kunal902770@gmail.com>
2023-05-24 22:36:27 +0530

    update

--- flask_tutorial/web.py
+++ flask_tutorial/web.py
@@ -10,20 +10,23 @@ def home():
 def document():
     return render_template("base.html")
 
-
-
-
-
-@app.route('/hello')
+@app.route('/hlo')
 def hello():
     return '<h1>Jai Shree Ram<h1> \n hello its page 2 '
 
 @app.route('/admin')
 def admin():
-    return redirect(url_for("hello"))  # redirect & url_for is used to redirect user from 1 url to other
-
+    return redirect(url_for("home"))  # redirect & url_for is used to redirect user from 1 url to other
 
 if __name__=="__main__":
     app.run(debug=True) 
 # when we use debug=True it auto start the server after adding new changes but never use this while your server is also used by some one else
 # port is used to change port of the web site 
+
+
+
+
+
+
+
+
