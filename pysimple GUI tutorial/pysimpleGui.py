2a81730b5e44444fa50e729169c0da4fec258f9a
Kunal9027 <kunal902770@gmail.com>
2023-06-06 22:32:20 +0530

    new up date

--- pysimple GUI tutorial/pysimpleGui.py
+++ pysimple GUI tutorial/pysimpleGui.py
@@ -5,15 +5,15 @@ sg.theme('lightyellow')   # Add a touch of color
 layout = [  [sg.Text('Hello')],
             [sg.Text('What is  your name ')], 
             [sg.InputText()],
-            [sg.Button('Ok'), sg.Button('Cancel')] ]
+            [sg.Button('Ok'), sg.Button('Exit')] ]
 
 # Create the Window
 window = sg.Window('Window Title', layout)
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+    if event == sg.WIN_CLOSED or event == 'Exit': # if user closes window or clicks cancel
         break
-    print('You entered ', values[0])
+    print('Your Name', values[0])
 
 window.close()
